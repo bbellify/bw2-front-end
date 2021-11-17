82276b8e0fabb3a76134a6fd5f99ace939a1400d
Dave Madden <davelearnsstuff@gmail.com>
2021-11-17 10:53:09 -0800

    logout changes

--- src/components/LogOut.js
+++ src/components/LogOut.js
@@ -1,18 +1,18 @@
 import React, { useEffect } from "react";
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import axiosWithAuth from "../utils/axiosWithAuth";
 
 const Logout = () => {      
-    const {push} = useHistory();
-
+    
+    const navigate = useNavigate();
     useEffect(()=> {
 
         axiosWithAuth()
             .post('/logout')
             .then(resp => {
                 localStorage.removeItem('token');
-                push('/login');
+                navigate('/login');
             });
     }, []);  
     return(<div></div>);
